bbdd7b3d4b27a5cbba9d1a003b6947eac737c564
Max Alcala <maxarturo@gmail.com>
2017-12-06 08:32:35 -0600

    fix 002, solve 002_2

--- 002_2_solution.hs
+++ 002_2_solution.hs
@@ -2,21 +2,27 @@ module Main where
 
 import System.IO
 import Data.String
-import Data.Char
+import Data.List
+-- import Debug.Trace
 
+findDiv :: [Integer] -> Integer -> Integer
+findDiv _ a | a /= 0 = a
+findDiv [] _ = 0
+findDiv (x:xs) _ = case find (\n -> x `rem` n == 0) xs of
+  Nothing -> findDiv xs 0
+  Just a  -> findDiv [] $ div x a
+  
 getDiff :: [String] -> Integer
-getDiff l = max - min
+getDiff l = findDiv (reverse $ sort digits) 0
   where
     stringToInt x = read x :: Integer
     digits = fmap stringToInt l 
-    max = maximum digits
-    min = minimum digits
 
 solve :: String -> IO ()
 solve fileName = do
     input <- readFile fileName 
     let rows = filter (\x -> x /= []) (fmap words $ lines input)
-    print $ foldl (\x y -> x + getDiff y) 0 rows 
+    print $ foldl (\x y ->  x + getDiff y) 0 rows 
 
 main :: IO ()
 main = solve "002_input.txt"
