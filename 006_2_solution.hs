e1c3a9cf6254ae8fc78ed95707c04886d5c12cfa
Ubuntu <ubuntu@ip-172-31-60-129.ec2.internal>
2017-12-25 14:09:28 +0000

    add 006 solutions

--- 006_2_solution.hs
+++ 006_2_solution.hs
@@ -4,7 +4,7 @@ import System.IO
 import Data.List
 import qualified Data.Map as Map
 import qualified Data.Sequence as S
-import Debug.Trace
+-- import Debug.Trace
 
 redistribute :: S.Seq Int -> Int -> Int -> S.Seq Int
 redistribute seq index 0 = seq
@@ -14,24 +14,23 @@ redistribute seq index rem = let
     seq' = S.update index (el + 1) seq
     in redistribute seq' (index + 1) (rem - 1)
 
-detect :: S.Seq Int -> Map.Map (S.Seq Int) Int -> Int 
-detect seq counts = let
+detect :: S.Seq Int -> Int -> Map.Map (S.Seq Int) Int -> Int
+detect seq currCount counts = let
   maxEl = maximum seq
   maxIndex = length $ S.takeWhileL (< maxEl) seq
   seq' = S.update maxIndex 0 seq
   result = redistribute seq' (maxIndex + 1) maxEl
-  in case Map.member result counts of
-    True -> 1 + Map.size counts
-    False -> traceShow result $ detect result $ Map.insert result 1 counts
+  in case Map.lookup result counts of
+    Just a  -> currCount - a + 1
+    Nothing -> detect result (currCount + 1) $ Map.insert result (currCount + 1) counts
 
 solve :: String -> IO ()
 solve fileName = do
   input <- readFile fileName 
   let list = S.fromList $ fmap (\x -> read x :: Int) $ concat $ fmap words $ lines input
   print list
-  print $ detect list Map.empty
+  print $ detect list 0 Map.empty
 
 main :: IO ()
--- main = solve "006_test.txt"
 main = solve "006_1_input.txt"
 
